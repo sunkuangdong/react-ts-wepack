5c6d97862009bffdc63b4cd4b3f765d6a61fef23
sunkuangdong <sunkuangdong@163.com>
2022-12-28 16:52:30 +0800

    配置ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,28 @@
-const path = require('path');
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+
+const smp = new SpeedMeasurePlugin();
+
+const webpackConfig = smp.wrap({
+  plugins: [new MyPlugin(), new MyOtherPlugin()],
+});
 module.exports = {
-    entry: './src/main.ts'
-}
+  entry: "./src/main.ts",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.[hash].js",
+    clean: true,
+  },
+  plugins: [
+    new BundleAnalyzerPlugin(),
+    new htmlWebpackPlugin({
+      title: "react web",
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
---
+++ webpack.develop.js
@@ -0,0 +1,6 @@
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+
+const smp = new SpeedMeasurePlugin();
+const webpackConfig = {}
+
+module.exports = webpackConfig
